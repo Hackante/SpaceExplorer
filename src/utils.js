624e053c52429c46b7163c638471ab67bcd99433
Hackante <kante.hagen@gmail.com>
2022-08-04 23:27:52 +0200

    exploration

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,32 @@
-module.exports = {
+const explorers = require("./Schemas/explorers");
+
+module.exports = class Util {
     // Resolve a color from Hex to number
-    resolveColor: function(color) {
+    static recolveColor(color) {
         return parseInt(color.replace("#", ""), 16);
+    }
+
+    // Add XP to a user and check it for level up
+    static async addXP(userID, xp) {
+        let explorer = await explorers.findOne({ user: userID });
+        if(!explorer) explorer = await explorers.create({ user: userID });
+        explorer.xp += xp;
+        // linear level up till level 5 then exponential
+        switch(explorer.level <= 5){
+            case true:
+                if(explorer.xp >= (explorer.level * 50)){
+                    explorer.xp -= (explorer.level * 50);
+                    explorer.level++;
+                }
+                break;
+            case false:
+                if(explorer.xp >= (Math.pow(explorer.level, 2) * 10)){
+                    explorer.xp -= (Math.pow(explorer.level, 2) * 10);
+                    explorer.level++;
+                }
+                break;
+        }
+        explorer.save();
+        return false;
     }
 }
