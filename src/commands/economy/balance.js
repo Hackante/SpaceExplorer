28b0700ee7dddbc095f55f8043477e216263c308
Hackante <kante.hagen@gmail.com>
2022-08-07 12:43:52 +0200

    balance

---
+++ src/commands/economy/balance.js
@@ -0,0 +1,31 @@
+module.exports = {
+    object: {
+        name: "balance",
+        description: "View your balance",
+        options: [
+            {
+                name: "user",
+                description: "The user to view the balance of",
+                type: 6,
+                required: false,
+            }
+        ]
+    },
+    run: async function(interaction, client) {
+        let user = interaction.options.getUser("user") || interaction.user;
+        let explorer = await explorers.findOne({ user: user.id });
+        if (!explorer) {
+            interaction.reply({content: interaction.i18n("inventory.noExplorer"), ephemeral: true});
+            return;
+        }
+        let embed = {
+            title: "Balance",
+            description: `${user} currently has ${explorer.inventory.credits} credits.`,
+            thumbnail: {
+                url: user.avatarURL()
+            },
+        }
+        interaction.reply({embeds: [embed]});
+        return;
+    }
+}
